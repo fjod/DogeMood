caf80e40b4a22f605aaba4f8c458fc38f2dc99f6
Михаил Снытко work <fjod10199@gmail.com>
2019-07-22 17:24:11 +0300

    reddit usage, token getter

---
+++ Doge/Utils/RedditPics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doge.Utils
+{
+    public class RedditPics : IGetPics
+    {
+        public RedditPics(IGetToken _token)
+        {
+            token = _token;
+        }
+        IGetToken token { get; }
+    }
+
+    public interface IGetPics
+    {
+    }
+}
